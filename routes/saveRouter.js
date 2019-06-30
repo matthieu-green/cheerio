a6b6384c0e9625db0404e5baa914128abf4c2f7f
Matthieu Green <matthieu.green@dauphine.eu>
2019-06-30 11:32:48 +0000

    made changes to idrc

--- routes/saveRouter.js
+++ routes/saveRouter.js
@@ -336,25 +336,17 @@ saveRouter.route('/')
           var a = $(this);
 
           var title = a.children('.views-field-title').children('.field-content').text()
-          var url = a.children('.views-field-title').children('.field-content').children('a').attr('href')
+          var url = 'https://www.idrc.ca/' + a.children('.views-field-title').children('.field-content').children('a').attr('href')
           var type = a.children().eq(1).text()
           var validite = a.children().eq(2).children('span').text()
           var info = "";
           var theme = "Non défini"
 
           if(type.toLowerCase().includes("proposals")){
-            var urlArray = title.split("’").join("").split(" ")
-            for(i = 0; i<urlArray.length;i++){
-              if(urlArray[i] == "in" || urlArray[i] == "on" || urlArray[i] == "to" || urlArray[i] == "of" || urlArray[i] == "or" || urlArray[i] == "and" || urlArray[i] == "for"){
-                urlArray.splice(i,1)
-              }
-            }
-            var uArray = urlArray.join("-")
-            var reqUrl = 'https://www.idrc.ca/en/funding/' + uArray
 
             request({method: "GET",
                     "rejectUnauthorized": false,
-                    "url": reqUrl}, function (error, response, html) {
+                    "url": url}, function (error, response, html) {
               if (!error && response.statusCode == 200) {
                 var $ = cheerio.load(html);
                 $('.view-display-id-panel_pane_2').each(function(i, element){
@@ -363,17 +355,18 @@ saveRouter.route('/')
                   info = b.text()
                   var metadata = {
                     title: title,
-                    url: reqUrl,
-                    validite: "Fin de validité: " + validite,
+                    url: url,
+                    validite: validite,
                     theme: theme,
                     financement: "International Development Research Center",
                     source: "idrc",
-                    info: info.split("\n\n       ").join("================================================")
+                    info: info.split("\n\n       ").join("============================")
                   }
                   table.push(metadata)
                 });
               }
             });
+
           }
 
         });
